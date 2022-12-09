640934fd704cff51438c45cfb4304dea7c4c3c7c
Kian Zareinajad <kzareinajad@gmail.com>
2022-12-09 14:15:02 +0000

    v1

--- src/app.js
+++ src/app.js
@@ -1,19 +1,44 @@
 require ('./db/connection');
 const yargs = require('yargs');
 const mongoose = require('mongoose'); //node --trace-deprecation ...
-const {createMovie} = require('./movies/function');
+const {createMovie, readMovies , updateActor, deleteTitle, updateDirector} = require('./movies/function');
+const MovieCollection = require('./movies/model');
 
 
 async function app(yargsinput) {
     if (yargsinput.create) {
         // put code here for create
-        await createMovie({title: yargsinput.title, actor: yargsinput.actor, director: yargsinput.director, rating: yargsinput.rating});
+        console.table();
+        await createMovie(
+            {title: yargsinput.title,
+                actor: yargsinput.actor, 
+                director: yargsinput.director,
+                rating: yargsinput.rating});
+    
     } else if (yargsinput.read) {
         // code for list all movies
-    } else if (yargsinput.update) {
+        console.table('Reading database');
+        await readMovies();
+    
+    } else if (yargsinput.updateActor) {
         //code here for updating actor
+        console.table('Updating actor');
+        await updateActor(yargsinput.title , yargsinput.actor)
+        
+    } else if (yargsinput.updateDirector) {
+        //code here for updating actor
+        console.table('Updating director');
+        await updateDirector(yargsinput.title , yargsinput.director)
+        
+    } else if (yargsinput.updateRating) {
+        //code here for updating actor
+        console.table('Updating rating');
+        await updateActor(yargsinput.title , yargsinput.rating)
+        
     } else if (yargsinput.delete) {
         //code for deleting a movie
+        console.log('deleting movie')
+        await deleteTitle (yargsinput.title )
     } else {
         console.log("Unrecognised option")
     }
--- src/movies/function.js
+++ src/movies/function.js
@@ -2,6 +2,7 @@ const MovieCollection = require("./model")
 
 async function createMovie(movieObject) {
     try {
+        console.log(movieObject)
         const newMovie = await MovieCollection.create(movieObject);
         console.log (newMovie);
     } catch (error) {
@@ -9,4 +10,57 @@ async function createMovie(movieObject) {
     }
 };
 
-module.exports = {createMovie};
+async function readMovies () {
+    try {
+        console.table('readMovies')
+        const readMovie = await MovieCollection.find({});
+        console.table(readMovie.toString())
+        for (let index = 0; index < readMovie.length; index++){
+            const element = readMovie[index];
+            console.table({"film": element.title, "director": element.director, "actor": element.actor,  "rating": element.rating})
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+async function updateActor (title, actor){
+    try {
+        const result = await MovieCollection.updateOne({title: title} , {$set: {actor: actor}});
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+async function updateDirector (title, director){
+    try {
+        const result = await MovieCollection.updateOne({title: title} , {$set: {director: director}});
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+async function updateRating (title, rating) {
+    try {
+        const result = await MovieCollection.updateOne({title: title} , {$set: {rating: rating}});
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+async function deleteTitle (title){
+    try {
+        const result = await MovieCollection.deleteOne({title: title});
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+
+module.exports = {createMovie , readMovies , updateActor, updateDirector, updateRating, deleteTitle};
